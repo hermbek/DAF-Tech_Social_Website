134918954e61a09198d7cd3dad8d09dc13fe4e99
hermbek <hermelabereket8@gmail.com>
2023-09-25 15:01:11 +0300

    admin

---
+++ Admin/src/api/testimonal/controllers/testimonal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * testimonal controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::testimonal.testimonal');
